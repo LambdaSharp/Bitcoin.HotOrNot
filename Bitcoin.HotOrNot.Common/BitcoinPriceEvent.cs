b915b2bb4edc225e9cb68524aa36a796b7bb7276
Steve Bjorg <steve.bjorg@gmail.com>
2020-12-08 12:19:47 -0800

    step2

---
+++ Bitcoin.HotOrNot.Common/BitcoinPriceEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Bitcoin.HotOrNot.Common {
+    public class BitcoinPriceEvent {
+
+        //--- Properties ---
+        public double Price { get; set; }
+    }
+
+    public class BitcoinVoteEvent {
+
+        //--- Properties ---
+        public string VoterId { get; set; }
+        public bool Vote { get; set; }
+    }
+}
